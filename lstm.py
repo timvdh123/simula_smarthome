cff386212842e3163bb24063258546c836318ef1
Tim van der Horst <timvanderhorst@gmail.com>
2020-07-11 14:32:16 +0200

    Create method to train models for individual sensors

--- lstm.py
+++ lstm.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn import metrics
+from sklearn.utils import class_weight
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.layers import Dense, LSTM
 from tensorflow.keras.models import Sequential
@@ -15,7 +16,7 @@ def prepare_data_single_output(d, window_size = 70, shift_direction=-1, dt=60, w
     if with_time:
         series_sensor_data['hour'] = series_sensor_data.index.hour
     data = np.zeros((len(series_sensor_data), window_size, series_sensor_data.shape[1]))
-    output = series_sensor_data.shift(shift_direction*window_size).values
+    output = series_sensor_data.loc[:, series_sensor_data.columns != 'hour'].shift(shift_direction*window_size).values
     for i in range(window_size):
         data[:, i, :] = series_sensor_data.shift(shift_direction*i)
     if shift_direction == 1:
@@ -52,9 +53,80 @@ def get_model(timesteps, n_features, lr):
     lstm_autoencoder.summary()
     return lstm_autoencoder
 
+def get_model_individual_sensor(timesteps, n_features, lr):
+    lstm_autoencoder = Sequential()
+    lstm_autoencoder.add(LSTM(10, activation='relu', input_shape=(timesteps, n_features),
+                              return_sequences=True))
+    lstm_autoencoder.add(LSTM(6, activation='relu', return_sequences=True))
+    lstm_autoencoder.add(LSTM(1, activation='relu'))
+    lstm_autoencoder.add(Dense(10, kernel_initializer='glorot_normal', activation='relu'))
+    lstm_autoencoder.add(Dense(10, kernel_initializer='glorot_normal', activation='relu'))
+    lstm_autoencoder.add(Dense(1, activation='sigmoid'))
+    adam = Adam(lr)
+    lstm_autoencoder.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+    lstm_autoencoder.summary()
+    return lstm_autoencoder
+
+def train_every_sensor(d, **kwargs):
+    window_size = kwargs.setdefault('window_size', 60) #Number of steps to look back
+    epochs = kwargs.setdefault('epochs', 20)
+    batch = kwargs.setdefault('batch', 24)
+    lr = kwargs.setdefault('lr', 0.0004)
+    dt = kwargs.setdefault('dt', 600)
+
+
+    X, y = prepare_data_single_output(d, **kwargs)
+    X_test = X[-2*(3600//dt)*24:]
+    X_validation = X[-4*(3600//dt)*24:-2*(3600//dt)*24]
+    X_train = X[:-4*(3600//dt)*24]
+
+    n_features = X.shape[2]  # 59
+    for index, id in enumerate(d.sensor_data.id.unique()):
+        model = get_model_individual_sensor(n_features=n_features, timesteps=window_size, lr=lr)
+        print("Training data shape %s" % str(X_train.shape))
+        y_train = y[:-4 * (3600 // dt) * 24, index]
+        y_test = y[-2 * (3600 // dt) * 24:, index]
+        y_validation = y[-4 * (3600 // dt) * 24:-2 * (3600 // dt) * 24, index]
+
+        cp = ModelCheckpoint(filepath="lstm_autoencoder_classifier_sensor_%d.h5" % id,
+                             verbose=0)
+        print("Sensor %d" % id)
+        weight = class_weight.compute_class_weight('balanced', classes=np.unique(y_train),
+                                                   y=y_train)
+        weight /= sum(weight)
+        weight = dict(enumerate(weight))
+        print(weight)
+        if weight[1] < 0.8:
+            weight = None
+        lstm_autoencoder_history = model.fit(X_train, y_train,
+                                             epochs=epochs,
+                                             batch_size=batch,
+                                             verbose=2,
+                                             validation_data=(X_validation, y_validation),
+                                             callbacks=[cp],
+                                             class_weight=weight
+                                             ).history
+        plt.plot(lstm_autoencoder_history['loss'], linewidth=2, label='Train')
+        plt.legend(loc='upper right')
+        plt.title('Model loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.show()
+        yPredTest = model.predict(X_test)
+        if len(yPredTest.shape) == 3:
+            yPredTest = yPredTest[:, 0, :].reshape(X_test.shape[0], X_test.shape[2])
+        yPredTest = (yPredTest > 0.5).astype(int)
+        print(metrics.classification_report(y_test, yPredTest))
+        fig, ax = plt.subplots()
+        ax.plot(y_test, label='Actual')
+        ax.plot(yPredTest, label='Predicted')
+        plt.title("Sensor %d" % id)
+        plt.show()
+
+
 def train(d, **kwargs):
     window_size = kwargs.setdefault('window_size', 60) #Number of steps to look back
-    epochs = kwargs.setdefault('epochs', 200)
+    epochs = kwargs.setdefault('epochs', 20)
     batch = kwargs.setdefault('batch', 24)
     lr = kwargs.setdefault('lr', 0.004)
     dt = kwargs.setdefault('dt', 600)
