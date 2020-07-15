b74a840cad5c152413ce3f45af8a8eae215c67dc
Tim van der Horst <timvanderhorst@gmail.com>
2020-07-15 11:23:09 +0200

    Try larger batch size, clipnorm

--- lstm.py
+++ lstm.py
@@ -10,8 +10,38 @@ from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from tensorflow.keras.layers import Dense, LSTM, Dropout, RepeatVector, TimeDistributed
 from tensorflow.keras.regularizers import l1, l2
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam, RMSprop
+from tensorflow.keras.optimizers import Adam, RMSprop, SGD
 from tensorflow.keras.utils import plot_model
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.callbacks import Callback
+
+
+class NBatchLogger(Callback):
+    """
+    A Logger that log average performance per `display` steps.
+    """
+    def __init__(self, display):
+        self.step = 0
+        self.display = display
+        self.metric_cache = {}
+
+    def on_batch_end(self, batch, logs={}):
+        self.step += 1
+        if np.isnan(logs.get('loss', 0)) or logs.get('loss', 0) > 100:
+            for k in logs:
+                self.metric_cache[k] = self.metric_cache.get(k, 0) + logs[k]
+            if self.step % self.display == 0:
+                metrics_log = ''
+                for (k, v) in self.metric_cache.items():
+                    val = v / self.display
+                    if abs(val) > 1e-3:
+                        metrics_log += ' - %s: %.4f' % (k, val)
+                    else:
+                        metrics_log += ' - %s: %.4e' % (k, val)
+                print('step: {}/{} ... {}'.format(self.step,
+                                              self.params['steps'],
+                                              metrics_log))
+                self.metric_cache.clear()
 
 def prepare_data_future_steps(d, window_size = 70, dt=60,
                                      with_time=False, future_steps=20, **kwargs):
@@ -128,7 +158,7 @@ def get_model_future_predictions_stack_vector(timesteps, future_timesteps, n_fea
     # model.add(LSTM(20, activation='relu', return_sequences=False))
     # model.add(Dense(20, kernel_initializer='glorot_normal', activation='relu'))
     model.add(Dense(future_timesteps, activation='sigmoid'))
-    adam = Adam(lr)
+    adam = Adam(lr, clipnorm=1)
     model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
     model.summary()
     plot_model(model, 'model.png', show_shapes=True)
@@ -141,10 +171,12 @@ def train_future_timesteps(d, **kwargs):
     future_steps = kwargs.setdefault('future_steps', int(window_size*0.2)) #Number of steps to look
     # back
     epochs = kwargs.setdefault('epochs', 20)
-    batch = kwargs.setdefault('batch', 5)
-    lr = kwargs.setdefault('lr', 0.0001)
+    batch = kwargs.setdefault('batch', 128)
+    lr = kwargs.setdefault('lr', 1e-5)
     dt = kwargs.setdefault('dt', 600)
     X, y = prepare_data_future_steps(d, **kwargs)
+    X = X.astype(int)
+    y = y.astype(int)
     X_train = X[:-2*(3600//dt)*24]
     X_test = X[-2*(3600//dt)*24:]
     n_features = X.shape[2]  # 59
@@ -168,11 +200,14 @@ def train_future_timesteps(d, **kwargs):
             except ValueError as v:
                 print("Could not load model weights")
         print("Sensor %d" % id)
+        out_batch = NBatchLogger(display=1)
+
         lstm_autoencoder_history = model.fit(X_train, y_train,
                                              epochs=epochs,
                                              batch_size=batch,
-                                             verbose=2,
-                                             callbacks=[cp],
+                                             verbose=4,
+                                             callbacks=[cp, out_batch],
+                                             shuffle=False,
                                              ).history
         plt.plot(lstm_autoencoder_history['loss'], linewidth=2, label='Train')
         plt.legend(loc='upper right')
